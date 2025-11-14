4b4ab045be53c95d074ef8dcf39123a246538117
Erick Gabriel <gabrielerick614@gmail.com>
2025-11-13 21:23:01 -0300

    Atualização views

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const __dirname = path.resolve();
 
 // EJS config
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views","./views");
 
 // Middlewares
 app.use(express.static(path.join(__dirname, "public")));
